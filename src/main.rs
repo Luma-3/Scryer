f3ce11980b4d7ef5a1692051a1da7a4e3f0a6f64
Luma-3 <jb.brs@icloud.com>
2026-04-20 13:58:23 +0200

    feat(PoC): auto launch sub-process ans inject LD_PRELOAD

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,63 @@
+use signal_hook::{consts::SIGINT, iterator::Signals};
 use std::os::unix::net::UnixDatagram;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::{env, io, thread};
+
+const SOCK_PATH: &str = "/tmp/scry.sock";
+
+fn open_socket() -> io::Result<UnixDatagram> {
+    let socket = UnixDatagram::bind(SOCK_PATH)?;
+    socket.set_nonblocking(true)?;
+    Ok(socket)
+}
 
 fn main() -> std::io::Result<()> {
-    let sock_path = "/tmp/scry.sock";
-    let socket = UnixDatagram::bind(sock_path)?;
     let mut buf = vec![0; 100];
 
-    lemt mut signal = Sign
+    let args: Vec<String> = env::args().collect();
 
-    println!("Start recv");
-    loop {
-        let size = socket
-            .recv(buf.as_mut_slice())
-            .expect("recv function failed");
-        let msg = std::str::from_utf8(&buf[..size]).expect("Failed to parse message");
-        println!("{}", msg);
+    let run = Arc::new(AtomicBool::new(true));
+    let run_clone = run.clone();
+
+    let mut signal = Signals::new([SIGINT])?;
+    thread::spawn(move || {
+        for sig in signal.forever() {
+            println!("Received signal {:?}", sig);
+            run_clone.store(false, Ordering::Release);
+        }
+    });
+
+    let socket = open_socket()?;
+
+    let output = std::process::Command::new(&args[1])
+        .envs([
+            ("LD_PRELOAD", "./spy/libspy.so"),
+            ("SCRY_SOCK_PATH", "/tmp/scry.sock"),
+        ])
+        .output()?;
+
+    println!("{:?}", std::str::from_utf8(output.stdout.as_slice()));
+    println!("Starting while");
+    while run.load(Ordering::Acquire) {
+        match socket.recv(buf.as_mut_slice()) {
+            Ok(size) => {
+                let msg = std::str::from_utf8(&buf[..size]).expect("Failed to parse message");
+                println!("{}", msg);
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                // No data available, just continue
+                continue;
+            }
+            Err(e) => {
+                eprintln!("Error receiving message: {}", e);
+                break;
+            }
+        }
     }
+    println!("Exiting...");
+    socket.shutdown(std::net::Shutdown::Both)?;
+    std::fs::remove_file(SOCK_PATH)?;
+
+    Ok(())
 }
